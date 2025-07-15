a3fcb0d7a4688a609cbc009bfd2225369aa32ff1
woogi7942 <30528739+woogi7942@users.noreply.github.com>
2025-07-15 09:52:14 +0900

    Add files via upload

---
+++ spring/250715/thymeleafLayout/src/main/java/home/HomeController.java
@@ -0,0 +1,15 @@
+package home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/index")
+	public String index(Model model) {
+		model.addAttribute("message","hello thymeleaf!!");
+		return "index"; //views/index.html
+	}
+}
